ad3e0d7bdc87b5733dec918bb284085461fd3511
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2026-06-16 23:54:55 -0400

    removed some extra css code and added some more javascript functionality

--- script.js
+++ script.js
@@ -2,43 +2,50 @@
 function verifyEmail() {
 
     // Values that are being used form this project
-    const emailInput = document.getElementById("emailInput").value;
+    const email = document.getElementById("emailInput").value;
     const output = document.getElementById("output");
 
     // An empty string value
-    if (emailInput === "") {
+    if (email === "") {
         window.confirm(`You haven't entered an email address, are you sure you want to continue?`);
         output.innerHTML = `Email cannot be blank!!!`;
+        document.getElementById("output").style.fontSize = `17px`;
+        document.getElementById("output").style.fontFamily = `Verdana, Geneva, Tahoma, sans-serif`;
+        document.getElementById("output").style.fontWeight = `bold`;
         output.style.color = `red`;
         return;
     }
 
     // Length fo the email address
-    if (emailInput.length < 5) {
+    if (email.length < 5) {
         window.confirm(`Are you sure that you want to validate your email address?`);
         output.innerHTML = `Email address is too short please try another one!!!`;
+        document.getElementById("output").style.fontSize = `17px`;
+        document.getElementById("output").style.fontFamily = `Verdana, Geneva, Tahoma, sans-serif`;
+        document.getElementById("output").style.fontWeight = `bold`;
         output.style.color = `red`;
         return;
     }
 
     // Checking for the @ symbol
-    if (!emailInput.includes('@')) {
+    if (!email.includes('@')) {
         window.confirm(`Are you sure that you want to validate your email address?`);
         output.innerHTML = `Email address does not have a @ symbol!!!`;
+        document.getElementById("output").style.fontSize = `17px`;
+        document.getElementById("output").style.fontFamily = `Verdana, Geneva, Tahoma, sans-serif`;
+        document.getElementById("output").style.fontWeight = `bold`;
         output.style.color = `red`;
         return;
     }
 
     // Checking for the email domain address
-    if(emailInput.endsWith === `@gmail.com` || `outlook.com` || `@yahoo.com`) {
+    if(email.endsWith === `@gmail.com` || `@outlook.com` || `@yahoo.com`) {
         window.confirm(`Validating your email address...`);
         output.innerHTML = `Congradulations, your email address is valid!!!`;
+        document.getElementById("output").style.fontSize = `17px`;
+        document.getElementById("output").style.fontFamily = `Verdana, Geneva, Tahoma, sans-serif`;
+        document.getElementById("output").style.fontWeight = `bold`;
         output.style.color = `green`;
-        return;
-    } else {
-        output.innerHTML = `Sorry your email address domain is invalid!!!`;
         return;
     }
 }
-
-window.alert(`Hello and welcome to my Email Verifcation program, please enter your email to make sure that you are using a valid email address.`);
